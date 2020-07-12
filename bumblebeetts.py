9ad1a3e37cdd861fd5c03aba52a5e04941dc18e5
Liam Keane <liamkeane6@yahoo.co.uk>
2020-07-12 18:26:00 +0100

    Now can find and download a song for every lyric
    Needs changing so it finds the lyric in the song and not just the title (sometimes other languages have english song names but none of the lyrics actually match)

--- bumblebeetts.py
+++ bumblebeetts.py
@@ -1,1 +1,101 @@
-using lyricsgenius
+import lyricsgenius
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import time
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout
+import pathlib
+import os
+import json
+import glob
+
+
+lyricPath = pathlib.Path(__file__).parent.absolute()
+lyricPath = os.path.join(lyricPath, "lyricFiles")
+
+if not os.path.exists(lyricPath):
+    os.mkdir(lyricPath)
+
+
+opts = Options()
+opts.set_headless()
+opts.add_argument('log-level=3')
+opts.add_experimental_option("prefs", {
+    "download.default_directory": lyricPath,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enable": True,
+    "profile.content_settings.exceptions.automatic_downloads.*.setting": True})
+assert opts.headless
+
+for f in os.listdir(lyricPath):
+    #try:
+    os.remove(os.path.join(lyricPath, f))
+    #except:
+       # pass
+
+sentence = "daisy daisy give me your answer do"
+
+data = {}
+      
+def on_button_clicked():
+    browser = Chrome(options=opts)
+    for word in sentence.split():
+        data['word'] = []
+        browser.get('https://www.megalobiz.com/lrc/maker/download-music-lyrics-lrc-generated-files')
+
+        search_form = browser.find_element_by_id('lrc_search_phrase')
+        search_form.send_keys(word)
+        time.sleep(2)
+        #results = browser.find_elements_by_class_name("lyrics_member_box")
+        lyricsList = browser.find_element_by_class_name("more_content")
+        for lyrics in lyricsList:
+            lyrics.click()
+            
+        
+        #print(results[0].text)       
+        
+
+        download_button = browser.find_elements_by_class_name("lyrics_button")
+        download_button[0].click()
+        time.sleep(1)
+        listofFiles = glob.glob(os.path.join(lyricPath, "*"))
+        latestFile = max(listofFiles, key=os.path.getctime)
+        
+        latestFile = latestFile.rsplit('\\', 1)[-1]
+        print(latestFile)
+
+        
+
+        #time.sleep(5)
+
+    # os.chdir(lyricPath)
+    # sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
+    # for f in os.listdir(lyricPath):
+    #     print(f)
+
+
+    
+        
+
+app = QApplication([])
+app.setStyle('Fusion')
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QLabel('Hello'))
+button = QPushButton('Speak')
+button.clicked.connect(on_button_clicked)
+layout.addWidget(button)
+window.setLayout(layout)
+window.show()
+app.exec_()
+
+
+
+
+
+
+
+
+#genius = lyricsgenius.Genius("lsBPwepMx8EwcFItbejjEY3JDghXUE8SIIndj03-CHFGtoXBTqOOZASvn_I9ntMw")
+#result = genius.search_genius_web("hello", 1)
---
+++ dictexample.py
@@ -0,0 +1,533 @@
+sections[
+	{
+		'type': 'top_hit',
+		'hits': [
+			{
+				'highlights': [
+				],
+				'index': 'song',
+				'type': 'song',
+				'result': {
+					'_type': 'song',
+					'annotation_count': 14,
+					'api_path': '/songs/2332455',
+					'full_title': 'Hello by\xa0Adele',
+					'header_image_thumbnail_url': 'https://images.genius.com/077abb7599bf12912ada980edde48651.300x169x1.jpg',
+					'header_image_url': 'https://images.genius.com/077abb7599bf12912ada980edde48651.1000x563x1.jpg',
+					'id': 2332455,
+					'instrumental': False,
+					'lyrics_owner_id': 672397,
+					'lyrics_state': 'complete',
+					'lyrics_updated_at': 1592397978,
+					'path': '/Adele-hello-lyrics',
+					'pyongs_count': 224,
+					'song_art_image_thumbnail_url': 'https://images.genius.com/56fe4a76d3a480e425824b2e18a2e983.300x300x1.jpg',
+					'song_art_image_url': 'https://images.genius.com/56fe4a76d3a480e425824b2e18a2e983.1000x1000x1.jpg',
+					'stats': {
+						'unreviewed_annotations': 0,
+						'concurrents': 2,
+						'hot': False,
+						'pageviews': 4883091
+					},
+					'title': 'Hello',
+					'title_with_featured': 'Hello',
+					'updated_by_human_at': 1592612878,
+					'url': 'https://genius.com/Adele-hello-lyrics',
+					'primary_artist': {
+						'_type': 'artist',
+						'api_path': '/artists/2300',
+						'header_image_url': 'https://images.genius.com/2a44416083dd36132512a635596d8589.1000x661x1.jpg',
+						'id': 2300,
+						'image_url': 'https://images.genius.com/2dfd665a4ae64d9e23534c1cca7b8a03.999x999x1.jpg',
+						'index_character': 
+'a',
+						'is_meme_verified': False,
+						'is_verified': False,
+						'name': 'Adele',
+						'slug': 'Adele',
+						'url': 'https://genius.com/artists/Adele'
+					}
+				}
+			},
+			{
+				'highlights': [
+					{
+						'property': 'lyrics',
+						'value': 'RGF productions\nRemy Boyz, yah-ah\n1738, ayy\nI\'m like "Hey, what\'s up? Hello" (Ayy)\nSeen yo pretty',
+						'snippet': True,
+						'ranges': [
+							{
+								'start': 70,
+								'end': 75
+							}
+						]
+					}
+				],
+				'index': 'lyric',
+				'type': 'song',
+				'result': {
+					'_type': 'song',
+					'annotation_count': 22,
+					'api_path': '/songs/496445',
+					'full_title': 'Trap Queen by\xa0Fetty\xa0Wap',
+					'header_image_thumbnail_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.300x300x1.jpg',
+					'header_image_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.640x640x1.jpg',
+					'id': 496445,
+					'instrumental': False,
+					'lyrics_owner_id': 104344,
+					'lyrics_state': 'complete',
+					'lyrics_updated_at': 1592699875,
+					'path': '/Fetty-wap-trap-queen-lyrics',
+					'pyongs_count': 3175,
+					'song_art_image_thumbnail_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.300x300x1.jpg',
+					'song_art_image_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.640x640x1.jpg',
+					'stats': {
+						'unreviewed_annotations': 0,
+						'concurrents': 6,
+						'hot': False,
+						'pageviews': 6754415
+					},
+					'title': 'Trap Queen',
+					'title_with_featured': 'Trap Queen',
+					'updated_by_human_at': 1592699875,
+					'url': 'https://genius.com/Fetty-wap-trap-queen-lyrics',
+					'primary_artist': {
+						'_type': 'artist',
+						'api_path': '/artists/216609',
+						'header_image_url': 'https://images.genius.com/cfdf0e6082f7216c2483b34c15420f43.700x393x1.jpg',
+						'id': 216609,
+						'image_url': 'https://images.genius.com/b0686e6e7eec842de90328ac63f4da85.400x400x1.jpg',
+						'index_character': 'f',
+						'is_meme_verified': True,
+						'is_verified': True,
+						'name': 'Fetty Wap',
+						'slug': 'Fetty-wap',
+						'url': 'https://genius.com/artists/Fetty-wap',
+						'iq': 100
+					}
+				}
+			},
+			{
+				'highlights': [
+				],
+				'index': 'artist',
+				'type': 'artist',
+				'result': {
+					'_type': 'artist',
+					'api_path': '/artists/48177',
+					'header_image_url': 'https://images.genius.com/a5a7c01460f2dd0d04254777f38788ca.1000x677x1.jpg',
+					'id': 48177,
+					'image_url': 'https://images.genius.com/5aa2fe53c5a1c4f885b19e2ef3b4ce1e.549x549x1.jpg',
+					'index_character': 'o',
+					'is_meme_verified': False,
+					'is_verified': False,
+					'name': 'The Oh Hellos',
+					'slug': 'The-oh-hellos',
+					'url': 'https://genius.com/artists/The-oh-hellos'
+				}
+			}
+		]
+	},
+	{
+		'type': 'song',
+		'hits': [
+			{
+				'highlights': [
+				],
+				'index': 'song',
+				
+'type': 'song',
+				'result': {
+					'_type': 'song',
+					'annotation_count': 14,
+					'api_path': '/songs/2332455',
+					'full_title': 'Hello by\xa0Adele',
+					'header_image_thumbnail_url': 'https://images.genius.com/077abb7599bf12912ada980edde48651.300x169x1.jpg',
+					'header_image_url': 'https://images.genius.com/077abb7599bf12912ada980edde48651.1000x563x1.jpg',
+					'id': 2332455,
+					'instrumental': False,
+					'lyrics_owner_id': 672397,
+					'lyrics_state': 'complete',
+					'lyrics_updated_at': 1592397978,
+					'path': '/Adele-hello-lyrics',
+					'pyongs_count': 224,
+					'song_art_image_thumbnail_url': 'https://images.genius.com/56fe4a76d3a480e425824b2e18a2e983.300x300x1.jpg',
+					'song_art_image_url': 'https://images.genius.com/56fe4a76d3a480e425824b2e18a2e983.1000x1000x1.jpg',
+					'stats': {
+						'unreviewed_annotations': 0,
+						'concurrents': 2,
+						'hot': 
+False,
+						'pageviews': 4883091
+					},
+					'title': 'Hello',
+					'title_with_featured': 'Hello',
+					'updated_by_human_at': 1592612878,
+					'url': 'https://genius.com/Adele-hello-lyrics',
+					'primary_artist': {
+						'_type': 'artist',
+						'api_path': '/artists/2300',
+						'header_image_url': 'https://images.genius.com/2a44416083dd36132512a635596d8589.1000x661x1.jpg',
+						'id': 2300,
+						'image_url': 'https://images.genius.com/2dfd665a4ae64d9e23534c1cca7b8a03.999x999x1.jpg',
+						'index_character': 'a',
+						'is_meme_verified': False,
+						'is_verified': False,
+						'name': 'Adele',
+						'slug': 'Adele',
+						'url': 'https://genius.com/artists/Adele'
+					}
+				}
+			}
+		]
+	},
+	{
+		'type': 'lyric',
+		'hits': [
+			{
+				'highlights': [
+					{
+						'property': 'lyrics',
+						'value': 'RGF productions\nRemy Boyz, yah-ah\n1738, ayy\nI\'m like "Hey, what\'s up? Hello" (Ayy)\nSeen yo pretty',
+						'snippet': True,
+						'ranges': [
+							{
+								'start': 70,
+								'end': 75
+							}
+						]
+					}
+				],
+				'index': 'lyric',
+				'type': 'song',
+				'result': {
+					'_type': 'song',
+					'annotation_count': 22,
+					'api_path': '/songs/496445',
+					'full_title': 'Trap Queen by\xa0Fetty\xa0Wap',
+					'header_image_thumbnail_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.300x300x1.jpg',
+					'header_image_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.640x640x1.jpg',
+					'id': 496445,
+					'instrumental': False,
+					'lyrics_owner_id': 104344,
+					'lyrics_state': 'complete',
+					'lyrics_updated_at': 1592699875,
+					'path': '/Fetty-wap-trap-queen-lyrics',
+					'pyongs_count': 3175,
+					'song_art_image_thumbnail_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.300x300x1.jpg',
+					'song_art_image_url': 'https://images.rapgenius.com/694274eb49f35ea1ad1acc36ab99390e.640x640x1.jpg',
+					'stats': {
+						'unreviewed_annotations': 0,
+						'concurrents': 6,
+						'hot': False,
+						'pageviews': 6754415
+					},
+					'title': 'Trap Queen',
+					'title_with_featured': 'Trap Queen',
+					'updated_by_human_at': 1592699875,
+					'url': 'https://genius.com/Fetty-wap-trap-queen-lyrics',
+					'primary_artist': {
+						'_type': 'artist',
+						'api_path': '/artists/216609',
+						'header_image_url': 'https://images.genius.com/cfdf0e6082f7216c2483b34c15420f43.700x393x1.jpg',
+						'id': 216609,
+						'image_url': 'https://images.genius.com/b0686e6e7eec842de90328ac63f4da85.400x400x1.jpg',
+						'index_character': 'f',
+						'is_meme_verified': True,
+						'is_verified': True,
+						'name': 'Fetty Wap',
+						'slug': 'Fetty-wap',
+						'url': 'https://genius.com/artists/Fetty-wap',
+						'iq': 100
+					}
+				}
+			}
+		]
+	},
+	{
+		'type': 'artist',
+		'hits': [
+			{
+				'highlights': [
+				],
+				'index': 'artist',
+				'type': 'artist',
+				'result': {
+					'_type': 'artist',
+					'api_path': '/artists/48177',
+					'header_image_url': 'https://images.genius.com/a5a7c01460f2dd0d04254777f38788ca.1000x677x1.jpg',
+					'id': 48177,
+					'image_url': 'https://images.genius.com/5aa2fe53c5a1c4f885b19e2ef3b4ce1e.549x549x1.jpg',
+					'index_character': 'o',
+					'is_meme_verified': False,
+					'is_verified': False,
+					'name': 'The Oh Hellos',
+					'slug': 'The-oh-hellos',
+					'url': 'https://genius.com/artists/The-oh-hellos'
+				}
+			}
+		]
+	},
+	{
+		'type': 'album',
+		'hits': [
+			{
+				'highlights': [
+				],
+				'index': 'album',
+				'type': 'album',
+				'result': {
+					'_type': 'album',
+					'api_path': '/albums/657',
+					'cover_art_thumbnail_url': 'https://images.genius.com/eb578f12527a926bd03de8035d8a7768.300x300x1.jpg',
+					'cover_art_url': 'https://images.genius.com/eb578f12527a926bd03de8035d8a7768.1000x1000x1.jpg',
+					'full_title': 'Hello Nasty by Beastie Boys',
+					'id': 657,
+					'name': 'Hello Nasty',
+					'name_with_artist': 'Hello Nasty (artist: Beastie Boys)',
+					'release_date_components': {
+						'year': 1998,
+						'month': 7,
+						'day': 14
+					},
+					'url': 'https://genius.com/albums/Beastie-boys/Hello-nasty',
+					'artist': {
+						'_type': 'artist',
+						'api_path': '/artists/329',
+						'header_image_url': 'https://images.genius.com/09c247c19326b24b53166c344eea1c91.1000x563x1.jpg',
+						'id': 329,
+						'image_url': 'https://images.genius.com/28a945e64fc4b3c0e6ce96486f6e4df3.450x450x1.jpg',
+						'index_character': 'b',
+						'is_meme_verified': False,
+						'is_verified': True,
+						'name': 'Beastie Boys',
+						'slug': 'Beastie-boys',
+						'url': 'https://genius.com/artists/Beastie-boys',
+						'iq': 100
+					}
+				}
+			}
+		]
+	},
+	{
+		'type': 'video',
+		'hits': [
+			{
+				'highlights': [
+				],
+				'index': 'video',
+				'type': 'video',
+				'result': {
+					'_type': 'video',
+					'api_path': '/videos/17334',
+					'article_url': 'https://genius.com/a/03-greedo-talks-2pac-influences-the-wolf-of-grape-street-on-for-the-record',
+					'author_list_for_display': 'Lisa Brown',
+					'dek': '"I never really could do nothing about the things that went wrong for me. I worked around it."',
+					'description': 'West Coast rapper 03 Greedo has gained momentum over the years with his three Purple Summer mixtapes and breakout track “Mafia Business,” a tribute to Greedo’s friend who was killed. After inking a new deal with Alamo Records, he returned this month…',
+					'dfp_kv': [
+						{
+							'name': 'brightcove_video_id',
+							'values': [
+								'5755766862001'
+							]
+						},
+						{
+							'name': 'topic',
+							'values': [
+								'weed'
+							]
+						},
+						{
+							'name': 'has_transcript',
+							'values': [
+								'true'
+							]
+						},
+						{
+							'name': 'song_id',
+							'values': [
+							]
+						},
+						{
+							'name': 'artist_id',
+							'values': [
+								'1186883'
+							]
+						},
+						{
+							'name': 'primary_tag_id',
+							'values': [
+								'1434'
+							]
+						},
+						{
+							'name': 'video_series',
+							'values': [
+							]
+						}
+					],
+					'duration': 1992240,
+					'id': 17334,
+					'poster_attributes': {
+						'height': 720,
+						'width': 1280
+					},
+					'poster_url': 'https://images.genius.com/e65fea4025e1f4c6be2731debcc5239f.1000x563x1.jpg',
+					'provider': 'brightcove',
+					'provider_id': '5755766862001',
+					'provider_params': [
+						{
+							'name': 'policy_key',
+							'value': 'BCpkADawqM1Yf2IElyF6jfI5WI9Q_UAa8Lz3mmqoN0btQzu-1LWMaRV4q6Tm7Fzh1LrovZfmf6rHqYXe_OdkLc9eTQiNrj2s_4t_gA0KfeiNKhrqNmRPrfQjTfgJRsGeyVoNA-qlxc9LBrtA'
+						}
+					],
+					'short_title': '03 Greedo Talks 2Pac, Influences & ‘The Wolf Of Grape Street’',
+					'title': '03 Greedo Talks 2Pac, Influences & ‘The Wolf Of Grape Street’ On ‘For The Record’',
+					'type': 'video',
+					'url': 'https://genius.com/videos/03-greedo-talks-2pac-influences-the-wolf-of-grape-street',
+					'video_attributes': {
+						'width': 1920,
+						'height': 1080
+					},
+					'current_user_metadata': {
+						'permissions': [
+						],
+						'excluded_permissions': [
+							'view_relevance_reason'
+						]
+					},
+					'published_at': 1521662040,
+					'view_count': 178647,
+					'author': {
+						'_type': 'user',
+						'about_me_summary': 'Staff Writer at Genius. Brooklyn native and resident cat lady.',
+						'api_path': '/users/5506789',
+						'avatar': {
+							'tiny': {
+								'url': 'https://images.genius.com/avatars/tiny/f4a7e20636cbe9b0456cae0600b9f40c',
+								'bounding_box': {
+									'width': 16,
+									'height': 16
+								}
+							},
+							'thumb': {
+								'url': 'https://images.genius.com/avatars/thumb/f4a7e20636cbe9b0456cae0600b9f40c',
+								'bounding_box': {
+									'width': 32,
+									'height': 32
+								}
+							},
+							'small': {
+								'url': 'https://images.genius.com/avatars/small/f4a7e20636cbe9b0456cae0600b9f40c',
+								'bounding_box': {
+									'width': 100,
+									'height': 100
+								}
+							},
+							'medium': {
+								'url': 'https://images.genius.com/avatars/medium/f4a7e20636cbe9b0456cae0600b9f40c',
+								'bounding_box': {
+									'width': 300,
+									'height': 400
+								}
+							}
+						},
+						'header_image_url': 'https://images.genius.com/avatars/medium/f4a7e20636cbe9b0456cae0600b9f40c',
+						'human_readable_role_for_display': 'Contributor',
+						'id': 5506789,
+						'iq': 4511,
+						'is_meme_verified': False,
+						'is_verified': False,
+						'login': 'lisambrown',
+						'name': 'Lisa Brown',
+						'role_for_display': 'contributor',
+						'url': 'https://genius.com/lisambrown',
+						'current_user_metadata': {
+							'permissions': [
+							],
+							'excluded_permissions': [
+								'follow'
+							],
+							'interactions': {
+								'following': False
+							}
+						}
+					},
+					'sponsorship': {
+						'_type': 'article',
+						'api_path': '/articles/4538',
+						'sponsor_image': None,
+						'sponsor_image_style': 'normal',
+						'sponsor_link': '',
+						'sponsor_phrase': None,
+						'sponsored': False
+					}
+				}
+			}
+		]
+	},
+	{
+		'type': 'article',
+		'hits': [
+			{
+				'highlights': [
+				],
+				'index': 'article',
+				'type': 'article',
+				'result': {
+					'_type': 'article',
+					'api_path': '/articles/3210',
+					'article_type': 'news',
+					'author_list_for_display': 'Chris Mench',
+					'dek': 'Anwar Jibawi’s “Cheating On Your Barber” skit has over 2 million views.',
+					'draft': False,
+					'featured_slot': None,
+					'for_homepage': True,
+					'for_mobile': True,
+					'id': 3210,
+					'preview_image': 'https://images.genius.com/c35ed1ff82e6db76889ef2390e105995.700x500x1.jpg',
+					'published_at': 1504118040,
+					'pyongs_count': 1,
+					'sponsor_image': None,
+					'sponsor_image_style': 'normal',
+					'sponsor_link': None,
+					'sponsor_phrase': None,
+					'sponsored': False,
+					'title': 'Cali Rapper Mike Darole’s “Hello” Is Blowing Up Thanks To A YouTube Comedian’s Viral Video',
+					'url': 'https://genius.com/a/cali-rapper-mike-daroles-hello-is-blowing-up-thanks-to-a-youtube-comedians-viral-video',
+					'votes_total': 8,
+					'author': {
+						'_type': 'user',
+						'about_me_summary': 'I’m the News Editor at Genius, currently living in NYC. One time Shia LaBeouf emailed me.\n\nKeep up with my latest articles on my official Facebook and Twitter pages.',
+						'api_path': '/users/3019515',
+						'avatar': {
+							'tiny': {
+								'url': 'https://images.genius.com/avatars/tiny/ef39c675b1b299469c8cec46c2f2b4eb',
+								'bounding_box': {
+									'width': 16,
+									'height': 16
+								}
+							},
+							'thumb': {
+								'url': 'https://images.genius.com/avatars/thumb/ef39c675b1b299469c8cec46c2f2b4eb',
+								'bounding_box': {
+									'width': 32,
+									'height': 32
+								}
+							},
+							'small': {
+								'url': 'https://images.genius.com/avatars/small/ef39c675b1b299469c8cec46c2f2b4f71',
+								'bounding_box': {
+									'width': 16,
+									'height': 16
+								}
+							},
+							'thumb': {
+								'url': 'https://images.genius.com/avatars/thumb/426d97d0e00a98500559b94ff0b34f71',
+								'bounding_box': {
+									'width': 32,
+									'height': 32
+								}
+							},
+							'small': {
+								'url': 'https://i'sponsor_image_style': 'normal', 'sponsor_lmages.genius.com/avatars/small/426d97d0e00a98500559b94ff0b34f71', 'bounding_box': {'width': 100, 'height': 100}}, 'medium': {'url': 'https: //images.genius.com/avatars/medium/426d97d0e00a98500559b94ff0b34f71', 'bounding_aboutwordsandmakingstuff.\n\nig: hellokbox': {'width': 300, 'height': 400}}}, 'header_image_url': 'https: //s3.amazonaws.com/filepicker-images-rapgenius/mwJ16CSPQLycx4udsbhr_picassopattern.png', 'human_readable_role_for_display': 'Editor', 'id': 2899000, 'iq'genius.com/avatars/thumb/426d97d0e00a985005: 7909, 'is_meme_verified': False, 'is_verified': False, 'login': 'hellokelcie', 'name': 'hellokelcie', 'role_for_display': 'editor', 'url': 'https: //genius.com/hellokelcie', 'current_user_metadata': {'permissions': [],': 'https://images.genius.com/avatars/mediu 'excluded_permissions': ['follow'], 'interactions': {'following': False}}}}]}]  
